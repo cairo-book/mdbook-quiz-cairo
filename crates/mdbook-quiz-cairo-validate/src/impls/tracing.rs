b2749ba461edf89e6d59aea0f3134848a99392e1
Mathieu <60658558+enitrat@users.noreply.github.com>
2024-04-20 22:40:35 +0200

    feat: mdbook quiz cairo support (#1)

--- crates/mdbook-quiz-cairo-validate/src/impls/tracing.rs
+++ crates/mdbook-quiz-cairo-validate/src/impls/tracing.rs
@@ -1,11 +1,52 @@
+use regex::Regex;
 use std::{
-  fs,
+  env::current_dir,
+  fs, io,
+  path::PathBuf,
   process::{Command, Stdio},
 };
 use tempfile::TempDir;
 
 use crate::{cxensure, tomlcast, SpannedValue, SpannedValueExt, Validate, ValidationContext};
 use mdbook_quiz_schema::*;
+
+use std::path::Path;
+
+fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> {
+  fs::create_dir_all(&dst)?;
+  for entry in fs::read_dir(src)? {
+    let entry = entry?;
+    let ty = entry.file_type()?;
+    if ty.is_dir() {
+      copy_dir_all(entry.path(), dst.as_ref().join(entry.file_name()))?;
+    } else {
+      fs::copy(entry.path(), dst.as_ref().join(entry.file_name()))?;
+    }
+  }
+  Ok(())
+}
+
+pub fn prepare_crate_for_exercise(file_path: &PathBuf) -> PathBuf {
+  // Prepare the crate for the exercise
+  let crate_path = current_dir().unwrap().join(PathBuf::from("runner_crate"));
+  let src_dir = crate_path.join("src");
+  if !src_dir.exists() {
+    let _ = fs::create_dir(&src_dir);
+  }
+
+  // Create a crate for this exercise only by copying the runner_crate directory and
+  // filling the src/lib.cairo file with the content of the file_path
+  let dest_crate_path = file_path.parent().unwrap().join("runner_crate");
+  copy_dir_all(&crate_path, &dest_crate_path).unwrap();
+
+  let lib_path = dest_crate_path.join("src").join("lib.cairo");
+  match fs::copy(file_path, &lib_path) {
+      Ok(_) => {}
+      Err(err) => panic!("Error occurred while preparing the quiz,\nQuiz: {file_path:?}\nLib path: {lib_path:?}\n{err:?}"),
+  };
+
+  dest_crate_path
+}
 
 impl Validate for Tracing {
   fn validate(&self, cx: &mut ValidationContext, value: &SpannedValue) {
@@ -16,21 +57,21 @@ impl Validate for Tracing {
     } = &self.0;
     let mut inner = || -> anyhow::Result<()> {
       let dir = TempDir::new()?;
-      let src_path = dir.path().join("main.rs");
+      let src_path = dir.path().join("main.cairo");
       fs::write(&src_path, program)?;
+      let crate_path = prepare_crate_for_exercise(&src_path);
 
-      let rustc_output = Command::new("rustc")
-        .arg(src_path)
-        .args(["-A", "warnings"])
+      let compile_output = Command::new("scarb")
+        .arg("build")
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
-        .current_dir(dir.path())
+        .current_dir(&crate_path)
         .output()?;
 
-      let rustc_stderr = String::from_utf8(rustc_output.stderr)?;
+      let scarb_stderr = String::from_utf8(compile_output.stderr)?;
       let answer_val = tomlcast!(value.table["answer"]);
 
-      if rustc_output.status.success() {
+      if compile_output.status.success() {
         cxensure!(
           cx,
           answer.does_compile,
@@ -45,13 +86,24 @@ impl Validate for Tracing {
           "program compiles but stdout is missing"
         );
 
-        let exe_path = dir.path().join("main");
-        let cmd_output = Command::new(exe_path)
+        let cmd_output = Command::new("scarb")
+          .arg("cairo-run")
+          .arg("--no-build")
           .stdout(Stdio::piped())
           .stderr(Stdio::piped())
-          .current_dir(dir.path())
+          .current_dir(crate_path)
           .output()?;
-        let cmd_stdout = String::from_utf8(cmd_output.stdout)?;
+
+        let cmd_stdout = {
+          let output = String::from_utf8(cmd_output.stdout)?;
+          let re =
+            Regex::new(r"Running runner_crate\s*(.*?)\s*Run completed successfully").unwrap();
+          match re.captures(&output) {
+            Some(caps) => caps[1].trim().to_string(),
+            None => output.trim().to_string(),
+          }
+        };
+
         let cmd_stderr = String::from_utf8(cmd_output.stderr)?;
 
         cxensure!(
@@ -76,8 +128,8 @@ impl Validate for Tracing {
           cx,
           !answer.does_compile,
           labels = vec![tomlcast!(answer_val.table["doesCompile"]).labeled_span()],
-          "program does not compile but doesCompile = true. rustc stderr:\n{}",
-          textwrap::indent(&rustc_stderr, "  ")
+          "program does not compile but doesCompile = true. scarb stderr:\n{}",
+          textwrap::indent(&scarb_stderr, "  ")
         );
 
         cxensure!(
@@ -108,6 +160,24 @@ answer.doesCompile = true
 answer.stdout = "Hello world"
 "#;
   assert!(crate::test::harness(contents).is_ok());
+}
+
+#[test]
+fn cairo_specific_validate_tracing_passes() {
+  let contents = r#"
+[[questions]]
+type = "Tracing"
+prompt.program = """
+fn main() {
+  let mut arr = array![1,2,3];
+  let x = arr.pop_front().unwrap();
+  println!("{x}");
+}
+"""
+answer.doesCompile = true
+answer.stdout = "1"
+"#;
+  assert!(crate::harness(contents).is_ok());
 }
 
 #[test]
--- crates/mdbook-quiz-cairo/src/main.rs
+++ crates/mdbook-quiz-cairo/src/main.rs
@@ -3,7 +3,7 @@ use mdbook_preprocessor_utils::{
   mdbook::preprocess::PreprocessorContext, Asset, HtmlElementBuilder, SimplePreprocessor,
 };
 
-use mdbook_quiz_validate::Validated;
+use mdbook_quiz_cairo_validate::Validated;
 use regex::Regex;
 use std::{
   env, fs,
@@ -127,7 +127,7 @@ impl QuizPreprocessor {
     let mut content_toml = fs::read_to_string(&quiz_path_abs)
       .with_context(|| format!("Failed to read quiz file: {}", quiz_path_abs.display()))?;
 
-    mdbook_quiz_validate::validate(
+    mdbook_quiz_cairo_validate::validate(
       &quiz_path_abs,
       &content_toml,
       &self.validated,
@@ -181,11 +181,11 @@ impl SimplePreprocessor for QuizPreprocessor {
   type Args = QuizArgs;
 
   fn name() -> &'static str {
-    "quiz"
+    "quiz-cairo"
   }
 
   fn build(ctx: &PreprocessorContext) -> Result<Self> {
-    log::info!("Running the mdbook-quiz preprocessor");
+    log::info!("Running the mdbook-quiz-cairo preprocessor");
 
     let config_toml = ctx.config.get_preprocessor(Self::name()).unwrap();
     let parse_bool = |key: &str| config_toml.get(key).map(|value| value.as_bool().unwrap());
@@ -206,7 +206,7 @@ impl SimplePreprocessor for QuizPreprocessor {
     };
 
     if let Some(more_words) = &config.more_words {
-      mdbook_quiz_validate::register_more_words(more_words)?;
+      mdbook_quiz_cairo_validate::register_more_words(more_words)?;
     }
 
     Ok(QuizPreprocessor {
